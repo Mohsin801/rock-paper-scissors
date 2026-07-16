31ed6fdae9ae08e33e4c164734452237ce8af41b
Muhammad Mohsin Hassan <97446313+Mohsin801@users.noreply.github.com>
2026-07-16 19:19:05 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from RPS_game import play, quincy, abbey, kris, mrugesh, human, random_player
+from RPS import player
+
+play(player, quincy, 1000)
+play(player, abbey, 1000)
+play(player, kris, 1000)
+play(player, mrugesh, 1000)
+
+# Uncomment the line below to run the unit tests
+# from unittest import main
+# main(module='test_module', exit=False)
+
